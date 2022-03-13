992f5038ac80934cd5e71f7b1da56ef58c416134
yasineryigit <yasineryigit737@gmail.com>
2022-03-13 14:55:07 +0300

    Orders listed | 2

--- app/src/main/java/com/ossovita/pronicdemo/adapter/OrderListAdapter.java
+++ app/src/main/java/com/ossovita/pronicdemo/adapter/OrderListAdapter.java
@@ -3,6 +3,7 @@ package com.ossovita.pronicdemo.adapter;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,11 +14,15 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.ossovita.pronicdemo.R;
 import com.ossovita.pronicdemo.model.Field;
 import com.ossovita.pronicdemo.model.Order;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrderListAdapter extends ArrayAdapter<Order> {
@@ -27,7 +32,6 @@ public class OrderListAdapter extends ArrayAdapter<Order> {
     private List<Order> orders;
     private List<Field> fields;
 
-
     public OrderListAdapter(@NonNull Context context, List<Order> orders, List<Field> fields) {
         super(context, R.layout.adapter_view_layout, orders);
         this.context = context;
@@ -36,6 +40,7 @@ public class OrderListAdapter extends ArrayAdapter<Order> {
         Log.d(TAG, "OrderListAdapter: kullanılacak fields:" + fields.size() + " orders: " + orders.size());
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -46,10 +51,8 @@ public class OrderListAdapter extends ArrayAdapter<Order> {
         Log.d(TAG, "getItemPosition order:" + order.toString());
         LayoutInflater inflater = LayoutInflater.from(context);
         if (convertView == null) {
-
             convertView = inflater.inflate(R.layout.adapter_view_layout, parent, false);
         }
-
 
         LinearLayout verticalLinearLayout = convertView.findViewById(R.id.verticalLinearLayout);
         LinearLayout.LayoutParams verticalLayoutparams = new LinearLayout.LayoutParams(
@@ -58,24 +61,35 @@ public class OrderListAdapter extends ArrayAdapter<Order> {
         );
         verticalLinearLayout.setOrientation(LinearLayout.VERTICAL);
         verticalLinearLayout.setLayoutParams(verticalLayoutparams);
-        verticalLinearLayout.setWeightSum(100);
+        verticalLinearLayout.setWeightSum(10f);
 
         LinearLayout.LayoutParams horizontalLayoutParams = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
         );
-        LinearLayout horizontalLinearLayout = new LinearLayout(getContext());
-        horizontalLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
-        horizontalLinearLayout.setLayoutParams(horizontalLayoutParams);
-        horizontalLinearLayout.setWeightSum(10f);
-        verticalLinearLayout.addView(horizontalLinearLayout);
+
+        Field element = Collections.max(fields, Comparator.comparingInt(Field::getRowNumber));
+        Log.d(TAG, "getView: max row:" + element.getRowNumber());
+
+        //kaç tane row varsa o kadar horizontal layout oluştur
+        for (int i = 0; i < element.getRowNumber(); i++) {
+            LinearLayout myHorizontalLinearLayout = new LinearLayout(getContext());
+            myHorizontalLinearLayout.setId(i);
+            myHorizontalLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            myHorizontalLinearLayout.setLayoutParams(horizontalLayoutParams);
+            myHorizontalLinearLayout.setWeightSum(10f);
+
+
+            verticalLinearLayout.addView(myHorizontalLinearLayout);
+
+        }
 
         for (Field field : fields) {//fieldları layout'a yerleştir
             TextView textView = new TextView(getContext());
             textView.setTextColor(Color.parseColor(field.getColor()));
             textView.setTypeface(null, field.isBold() ? Typeface.BOLD : Typeface.NORMAL);
 
-
+            //setText
             if (field.getType().equals("Static")) {
                 textView.setText(field.getName());
             } else if (field.getType().equals("Data")) {
@@ -91,18 +105,27 @@ public class OrderListAdapter extends ArrayAdapter<Order> {
                         break;
                 }
             }
-            if (field.getRowNumber() == 1) {
+            //hazırlanan textView'i ilgili horizontalLayout'a ekle
+            textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 10f / fields.size()));
+            Log.d(TAG, "getView: vertical layout child number:" + verticalLinearLayout.getChildCount());
+            LinearLayout linearLayout = (LinearLayout) verticalLinearLayout.getChildAt(field.getRowNumber() - 1);
+            System.out.println("addView linlayout:" + linearLayout);
+            linearLayout.addView(textView);
+
+
+            /*if (field.getRowNumber() == 1) {
                 horizontalLinearLayout.addView(textView);
                 textView.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 10f / fields.size()));
             } else if (field.getRowNumber() == 2) {
                 verticalLinearLayout.addView(textView);
                 textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 10f / fields.size()));
-            }
+            }*/
+
         }
 
+        //horizontal layout list elemanlarını vertical linear layout'a ekle
 
 
-
-        return convertView;
+        return verticalLinearLayout;
     }
 }
