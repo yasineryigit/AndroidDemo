9b846b661be86619456fc2ad74e692a1a8c4b497
yasineryigit <yasineryigit737@gmail.com>
2022-03-21 16:51:32 +0300

    .

--- app/src/main/java/com/ossovita/pronicdemo/adapter/OrderListAdapter.java
+++ app/src/main/java/com/ossovita/pronicdemo/adapter/OrderListAdapter.java
@@ -106,8 +106,6 @@ public class OrderListAdapter extends ArrayAdapter<OrderData> {
                             } catch (IllegalAccessException e) {
                                 e.printStackTrace();
                             }
-
-
                         }
                     }
 
